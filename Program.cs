2fbad46014c22b632f8429a892b280ac3ccb8624
João Matheus Marques <100247222+joaomatheus23@users.noreply.github.com>
2024-09-17 23:48:23 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿string login, senha;
+int cont = 1;
+
+Console.WriteLine("Digite seu login");
+login = Console.ReadLine();
+do
+{
+    if (login != "Admin")
+    {
+        Console.WriteLine("Login incorreto  redigite...");
+        login = Console.ReadLine();
+        cont++;
+    }
+    else
+    {
+        break;
+    }
+} while (cont < 3);
+if (cont == 3 && login != "Admin")
+{
+    Console.WriteLine("Você estourou seu maximo de tentativas");
+    Console.ReadKey();
+}
+else
+{
+    Console.WriteLine("Digite sua senha");
+    senha = Console.ReadLine();
+    cont = 1;
+
+    do
+    {
+        if (senha != "Senha123")
+        {
+            Console.WriteLine("Senha incorreto  redigite...");
+            senha = Console.ReadLine();
+            cont++;
+        }
+        else
+        {
+            break;
+        }
+    } while (cont < 3);
+    if (cont == 3 && senha != "Senha123")
+    {
+        Console.WriteLine("Você estourou seu maximo de tentativas");
+    }
+    else
+    {
+        Console.WriteLine("Parabens pelo minimo que é lembrar sua senha");
+    }
+
+}
